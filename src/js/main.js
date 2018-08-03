7dab3ebcc3608413d5ca763ce1a6991e60e3a728
Regie <regilyn.feliciano@gmail.com>
2018-08-03 01:30:33 -0600

    attempt to commit

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+$("body").load("../img/")
+$( document ).ready(function() {
+  $('.slider').slider({
+    indicators: true, height:600
+  });
+
+  // $('.carousel.carousel-slider').carousel({indicators: true, padding:200});
+  // setTimeout(autoplay, 4500);
+  // function autoplay() {
+  //   $('.carousel').carousel('next');
+  //   setTimeout(autoplay, 4500);
+  // }
+});
